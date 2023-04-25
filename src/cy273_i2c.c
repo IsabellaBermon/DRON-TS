e0f587a9f098a7f004d232a55767252d54b52209
Daniela C <daniela.cuartasm@udea.edu.co>
2023-04-24 22:24:40 -0500

    Se agrega magnetometro, falta calibrar

--- src/cy273_i2c.c
+++ src/cy273_i2c.c
@@ -2,5 +2,22 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 #include "hardware/i2c.h"
+#include "cy273.h"
 
+void cy273_reset(){
+    uint8_t data[1] = {0x3C};
+    i2c_write_blocking(i2c0, cy273_addr, data, sizeof(data), true);
+}
 
+void cy273_read(int16_t magneto[3]){
+    uint8_t buffer[6];
+
+    uint8_t val = 0x03;
+
+    i2c_write_blocking(cy273_i2c, cy273_addr, &val, sizeof(val), true);
+    i2c_read_blocking(cy273_i2c, cy273_addr, buffer, sizeof(buffer), false);
+
+    magneto[0] = (buffer[0] << 8) | buffer[1];  //x
+    magneto[1] = (buffer[2] << 8) | buffer[3];  //z
+    magneto[2] = (buffer[4] << 8) | buffer[5];  //y
+}
--- src/main.c
+++ src/main.c
@@ -3,27 +3,36 @@
 #include "hardware/gpio.h"
 #include "hardware/i2c.h"
 #include "MPU6050_i2c.h"
+#include "cy273.h"
 
-#define SDA 18
-#define SCL 19
-
+#define SDA_MPU 18 //pin 24
+#define SCL_MPU 19 //pin 25
+#define SDA_cy 8 //pin 11
+#define SCL_cy 9 //pin 12
 
 int main()
 {
     stdio_init_all();
 
     i2c_init(MPU6050_i2c, 100 * 1000);
-    gpio_set_function(SCL, GPIO_FUNC_I2C);
-    gpio_set_function(SDA, GPIO_FUNC_I2C);
-    gpio_pull_up(SCL);
-    gpio_pull_up(SDA);
+    i2c_init(cy273_i2c, 100 * 1000);
+    gpio_set_function(SCL_MPU, GPIO_FUNC_I2C);
+    gpio_set_function(SDA_MPU, GPIO_FUNC_I2C);
+    gpio_set_function(SCL_cy, GPIO_FUNC_I2C);
+    gpio_set_function(SDA_cy, GPIO_FUNC_I2C);
+    gpio_pull_up(SCL_MPU);
+    gpio_pull_up(SDA_MPU);
+    gpio_pull_up(SCL_cy);
+    gpio_pull_up(SDA_cy);
 
     mpu6050_reset();
+    cy273_reset();
 
-    int16_t acceleration[3], gyro[3], temp;
+    int16_t acceleration[3], gyro[3], temp, magneto[3];
 
     while (1) {
         mpu6050_read_raw(acceleration, gyro, &temp);
+        cy273_read(magneto);
         // These are the raw numbers from the chip, so will need tweaking to be really useful.
         // See the datasheet for more information
         printf("Acc. X = %d, Y = %d, Z = %d\n", acceleration[0], acceleration[1], acceleration[2]);
@@ -31,6 +40,7 @@ int main()
         // Temperature is simple so use the datasheet calculation to get deg C.
         // Note this is chip temperature.
         printf("Temp. = %f\n", (temp / 340.0) + 36.53);
+        printf("Magne. X = %d, Y = %d, Z = %d\n", magneto[0], magneto[2], magneto[1]);
 
         sleep_ms(1000);
     }
